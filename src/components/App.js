ea9f37619890d09a6ec3fb0d87d64d02ff9e27f4
Stephen Zhang <stzhang@guidewire.com>
2021-05-06 23:55:10 +1200

    create components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,14 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
+import utils from '../math-utils';
+import StarsDisplay from './StarsDisplay';
+import PlayNumber from './PlayNumber';
+import PlayAgain from './PlayAgain';
 
-export function App({ initialData }) {
-  const [count, setCount] = React.useState(0);
-  return (
-    <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button title="increment" onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
-    </div>
-  );
+const StarMatch = () => {
+  const [gameId, setGameId] = useState(1);
+  return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
+};
+
+export function App() {
+  return <StarMatch />;
 }
